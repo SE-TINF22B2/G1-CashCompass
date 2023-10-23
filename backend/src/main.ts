978908e3be8ad1068e028023e8c17d2383f882fc
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-10-24 00:30:48 +0200

    add global exception handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaModel } from './_gen/prisma-class';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  //Swagger
   const config = new DocumentBuilder()
     .setTitle('CashCompass')
     .setDescription('The CashCompass API description')
