796bf5f5dac2fb3cbeda7196494fac1fa05d9582
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-10-29 03:07:17 +0100

    make tests run

--- backend/src/mock-entity/mock-entity.service.spec.ts
+++ backend/src/mock-entity/mock-entity.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MockEntityService } from './mock-entity.service';
+import { PrismaService } from 'nestjs-prisma';
 
 describe('MockEntityService', () => {
   let service: MockEntityService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MockEntityService],
+      providers: [MockEntityService, PrismaService],
     }).compile();
 
     service = module.get<MockEntityService>(MockEntityService);
