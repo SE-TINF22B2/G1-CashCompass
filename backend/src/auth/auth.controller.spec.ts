f2b99c1a05ecab6edc2ff83cfdb57f3a93e8cd22
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-11-03 16:26:27 +0100

    make tests run again

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'nestjs-prisma';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +11,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthService, PrismaService, ConfigService, JwtService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
