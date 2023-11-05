87a8302da8f94db5bf2b10aaf6d99ecabb8c3e49
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-11-05 16:26:35 +0100

    add auth controller tests

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,23 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaService } from 'nestjs-prisma';
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
+import { AuthDto } from './dto';
+
+const mockAuthService = {
+  signup: jest.fn(),
+  signin: jest.fn(),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, PrismaService, ConfigService, JwtService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call authService.signup with the provided dto', async () => {
+    const authDto: AuthDto = {
+      email: 'test@example.com',
+      password: 'password123',
+    };
+
+    await controller.signup(authDto);
+
+    expect(authService.signup).toHaveBeenCalledWith(authDto);
+  });
+
+  it('should call authService.signin with the provided dto', async () => {
+    const authDto: AuthDto = {
+      email: 'test@example.com',
+      password: 'password123',
+    };
+
+    await controller.signin(authDto);
+
+    expect(authService.signin).toHaveBeenCalledWith(authDto);
   });
 });
