001212e70bf8236c93ca68606fb8a5e88bb3339c
Niklas Opiela <niklas.opiela@sap.com>
2023-11-13 15:09:22 +0100

    added structure of rest wrapper

---
+++ frontend/cashcompass_hook/lib/src/connector/rest_client.dart
@@ -0,0 +1,47 @@
+import 'package:cashcompass_hook/src/connector/error_handler.dart';
+import 'package:http/http.dart' as http;
+
+class RestClient {
+  final String baseUrl;
+  final String? port;
+  RestClient({required this.baseUrl, this.port});
+
+  String get _basetUrl => baseUrl + (port ?? "");
+
+  Future<http.Response> get(String path,
+      {Map<String, String>? headers, ErrorHandler? errorHandler}) async {
+    final response =
+        await http.get(Uri.parse('$_basetUrl$path'), headers: headers);
+    (errorHandler ?? const ErrorHandler()).handle(response);
+    return response;
+  }
+
+  Future<http.Response> post(String path, String body,
+      {Map<String, String>? headers, ErrorHandler? errorHandler}) async {
+    final response = await http.post(
+      Uri.parse('$_basetUrl$path'),
+      body: body,
+      headers: headers,
+    );
+    (errorHandler ?? const ErrorHandler()).handle(response);
+    return response;
+  }
+
+  Future<http.Response> put(String path, String body,
+      {Map<String, String>? headers, ErrorHandler? errorHandler}) async {
+    final response = await http.put(
+      Uri.parse('$_basetUrl$path'),
+      body: body,
+      headers: headers,
+    );
+    return response;
+  }
+
+  Future<http.Response> delete(String path,
+      {Map<String, String>? headers, ErrorHandler? errorHandler}) async {
+    final response =
+        await http.delete(Uri.parse('$_basetUrl$path'), headers: headers);
+    (errorHandler ?? const ErrorHandler()).handle(response);
+    return response;
+  }
+}
