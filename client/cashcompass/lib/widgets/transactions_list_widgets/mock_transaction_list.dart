0677e3dafbf3a3e526b5fa086d2d83ad5015cc8a
Mathis Imming <mathis.imming@sap.com>
2024-05-26 22:36:22 +0200

    adjust mocking to satisfy changes in hook

--- client/cashcompass/lib/widgets/transactions_list_widgets/mock_transaction_list.dart
+++ client/cashcompass/lib/widgets/transactions_list_widgets/mock_transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:cashcompass_hook/src/accounts/active_account/active_account.dart';
 import 'package:cashcompass_hook/src/accounts/category/category.dart';
-import 'package:cashcompass_hook/src/data_storage/accoutmanager.dart';
+import 'package:cashcompass_hook/src/data_storage/accout_manager.dart';
 import 'package:cashcompass_hook/src/transactions/transactions/transactions_factory.dart';
 
 class MockTransactions {
@@ -12,20 +12,20 @@ class MockTransactions {
           .create(
               amount: 100.00,
               soll: Category("Geschenk", 1, "#666666", "GiftsDonations"),
-              haben: ActiveAcount("ING", 2),
+              haben: ActiveAccount("ING", 2),
               label: "Geschenk")
           .build(),
       fac
           .create(
               amount: 6.50,
-              soll: ActiveAcount("Sparkasse", 3),
+              soll: ActiveAccount("Sparkasse", 3),
               haben: Category("Food", 4, "#666666", "Groceries"),
               label: "Döner")
           .build(),
       fac
           .create(
               amount: 25.00,
-              soll: ActiveAcount("ING", 5),
+              soll: ActiveAccount("ING", 5),
               haben: Category("Mobilität", 6, "#666666", "Transport"),
               label: "Fahrtkosten für Rudi")
           .build(),
@@ -33,14 +33,14 @@ class MockTransactions {
           .create(
               amount: 50.00,
               soll: Category("Geschenk", 7, "#666666", "GiftsDonations"),
-              haben: ActiveAcount("BAR", 8),
+              haben: ActiveAccount("BAR", 8),
               label: "Putzen bei Omi")
           .build(),
       fac
           .create(
               amount: 450.00,
               soll: Category("Arbeit", 9, "#666666", "Loans"),
-              haben: ActiveAcount("ING", 10),
+              haben: ActiveAccount("ING", 10),
               label: "Gehalt")
           .build(),
     ];
