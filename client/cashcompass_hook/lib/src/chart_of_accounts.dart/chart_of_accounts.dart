4dbfd6344780ccaca0e00061faf9806ea47b3d86
Niklas Opiela <niklas.opiela2003@gmail.com>
2024-05-29 09:26:15 +0200

    feat(shared): 255 Impl of Quarries

--- client/cashcompass_hook/lib/src/chart_of_accounts.dart/chart_of_accounts.dart
+++ client/cashcompass_hook/lib/src/chart_of_accounts.dart/chart_of_accounts.dart
@@ -1,5 +1,13 @@
 import 'package:cashcompass_hook/src/accounts/category/category.dart';
 import 'package:cashcompass_hook/src/data_storage/accout_manager.dart';
+import 'package:cashcompass_hook/src/transactions/transactions/transaction.dart';
+
+class CategoryAndTransactions {
+  final Category category;
+  final List<Transaction> soll, haben;
+  CategoryAndTransactions(
+      {required this.category, required this.haben, required this.soll});
+}
 
 class ChartOfAccounts {
   final Accountmanager _accountmanager;
@@ -10,5 +18,15 @@ class ChartOfAccounts {
         .getAllCategories()
         .where((category) => matcher != null ? matcher(category) : true)
         .toList();
+  }
+
+  List<CategoryAndTransactions> getCategoriesAndTransaktions() {
+    var cates = _accountmanager.getAllCategories();
+    var ret = <CategoryAndTransactions>[];
+    for (Category i in cates) {
+      ret.add(
+          CategoryAndTransactions(category: i, haben: i.habenT, soll: i.sollT));
+    }
+    return ret;
   }
 }
