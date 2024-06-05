0794abc8d44b11ded97ebd8e173a595d5184957a
stefan <54106627+stefanrudi2004@users.noreply.github.com>
2024-06-05 11:07:16 +0200

    stefanrudi2004/UI/108 Create Bottom Navigation Bar (#273)
    * Small changes
    
    * Added GoRouter
    
    * Update home_screen.dart
    
    ---------
    
    Co-authored-by: Stefan Rudi <stefan.rudi@sap.com>

--- client/cashcompass/lib/screens/my_app/my_app.dart
+++ client/cashcompass/lib/screens/my_app/my_app.dart
@@ -1,7 +1,7 @@
-import 'package:cashcompass/widgets/balance_overview/balance_overview.dart';
-import 'package:cashcompass/widgets/balance_overview/mock_transaction.dart';
+import 'package:cashcompass/screens/home_screen/home_screen.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:cashcompass/screens/auth_screen/auth_screen.dart';
+import 'package:cashcompass/screens/auth/auth_screen.dart';
+import 'package:go_router/go_router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,13 +9,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-        title: 'Flutter Demo',
-        theme: CupertinoThemeData(),
-        home: CupertinoPageScaffold(
-          child: SafeArea(
-            child: AuthScreen(),
-          ),
-        ));
+    return CupertinoApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    );
   }
 }
+
+//TODO: Replace with real Authentication logic
+bool isLoggedIn = true;
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/2', // Startet mit TransactionScreen
+  routes: [
+    GoRoute(
+      path: '/auth',
+      builder: (context, state) => const AuthScreen(),
+    ),
+    GoRoute(
+      path: '/',
+      redirect: (context, state) => isLoggedIn ? null : '/auth',
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: ':index',
+          builder: (context, state) {
+            final index = int.parse(state.pathParameters['index']!);
+            return HomeScreen(initialIndex: index);
+          },
+        ),
+      ],
+    ),
+  ],
+);
