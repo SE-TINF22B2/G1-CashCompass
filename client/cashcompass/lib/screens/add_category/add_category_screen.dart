70df93033b27640fe6d690cff726008860300c1b
Mathis Imming <mathis.imming@sap.com>
2024-06-12 12:11:32 +0200

    icon picker

--- client/cashcompass/lib/screens/add_category/add_category_screen.dart
+++ client/cashcompass/lib/screens/add_category/add_category_screen.dart
@@ -1,23 +1,58 @@
+import 'package:cashcompass_hook/src/accounts/category/category_icons.dart';
 import 'package:flutter/cupertino.dart';
 
 class AddCategoryScreen extends StatefulWidget {
-  const AddCategoryScreen({super.key});
+  const AddCategoryScreen({Key? key}) : super(key: key);
 
   @override
   State<AddCategoryScreen> createState() => _AddCategoryScreenState();
 }
 
 class _AddCategoryScreenState extends State<AddCategoryScreen> {
+  CategoryIcons selectedIcon = CategoryIcons.values[0];
+
+  _showPicker() async {
+    await showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          padding: EdgeInsets.all(8.0),
+          color: CupertinoColors.white, // non-transparent background
+          child: CupertinoPicker(
+            itemExtent: 35,
+            onSelectedItemChanged: (index) {
+              setState(() {
+                selectedIcon = CategoryIcons.values[index];
+              });
+            },
+            children: CategoryIcons.values
+                .map((icon) => Row(
+                      children: [
+                        Icon(icon.icon),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(icon.name),
+                      ],
+                    ))
+                .toList(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text("Add Category"),
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text("Add Category"),
       ),
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
+          child: ListView(
             children: [
               const Padding(
                 padding: EdgeInsets.symmetric(vertical: 8.0),
@@ -36,7 +71,7 @@ class _AddCategoryScreenState extends State<AddCategoryScreen> {
                   ],
                 ),
               ),
-              const Padding(
+              Padding(
                 padding: EdgeInsets.symmetric(vertical: 8.0),
                 child: Row(
                   children: [
@@ -45,11 +80,18 @@ class _AddCategoryScreenState extends State<AddCategoryScreen> {
                     ),
                     Text("Icon"),
                     SizedBox(
-                      width: 58,
+                      width: 50,
                     ),
                     Expanded(
-                      child: CupertinoTextField(),
-                    )
+                      child: CupertinoButton(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Icon(selectedIcon.icon)
+                            ]), // aligned to the left
+                        onPressed: _showPicker,
+                      ),
+                    ),
                   ],
                 ),
               ),
