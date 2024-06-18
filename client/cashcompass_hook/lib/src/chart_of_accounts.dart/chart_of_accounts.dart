ab1ed8e2a167c1a099cb7448a14b9f0579e8ecad
Gamagu <60237453+Gamagu@users.noreply.github.com>
2024-06-18 17:03:57 +0200

    255 fail querry collection new (#290)
    * feat(shared): 255 Impl of Quarries
    
    * feat(shared): 255 added expenses and income per category querry
    
    * feat(shared): 255 Added quarry for issue 87
    
    * feat(shared): 255 Impl of Quarries
    
    * feat(shared): 255 added expenses and income per category querry
    
    * feat(shared): 255 Added quarry for issue 87
    
    * feat(shared): 255 Added quarry for issue 87

--- client/cashcompass_hook/lib/src/chart_of_accounts.dart/chart_of_accounts.dart
+++ client/cashcompass_hook/lib/src/chart_of_accounts.dart/chart_of_accounts.dart
@@ -1,6 +1,15 @@
 import 'package:cashcompass_hook/src/accounts/active_account/active_account.dart';
 import 'package:cashcompass_hook/src/accounts/category/category.dart';
+import 'package:cashcompass_hook/src/chart_of_accounts.dart/data.dart';
 import 'package:cashcompass_hook/src/data_storage/accout_manager.dart';
+import 'package:cashcompass_hook/src/transactions/transactions/transaction.dart';
+
+class CategoryAndTransactions {
+  final Category category;
+  final List<Transaction> soll, haben;
+  CategoryAndTransactions(
+      {required this.category, required this.haben, required this.soll});
+}
 
 class ChartOfAccounts {
   final Accountmanager _accountmanager;
@@ -11,6 +20,58 @@ class ChartOfAccounts {
         .getAllCategories()
         .where((category) => matcher != null ? matcher(category) : true)
         .toList();
+  }
+
+  List<CategoryAndTransactions> getCategoriesAndTransaktions() {
+    var cates = _accountmanager.getAllCategories();
+    var ret = <CategoryAndTransactions>[];
+    for (Category i in cates) {
+      ret.add(
+          CategoryAndTransactions(category: i, haben: i.habenT, soll: i.sollT));
+    }
+    return ret;
+  }
+
+  Map<Category, Iterable<Expense>> getExpencesPerCategory(Category? category) {
+    Map<Category, Iterable<Expense>> ret = {};
+    Iterable<Category> categories = _accountmanager
+        .getAllCategories()
+        .where((cate) => category == null ? true : cate == category);
+
+    for (var category in categories) {
+      var e = category.sollT.map((elem) => Expense(elem, category));
+      if (e.isNotEmpty) {
+        ret[category] = e;
+      }
+    }
+    return ret;
+  }
+
+  Map<Category, Iterable<Income>> getIncomePerCategory(Category? category) {
+    Map<Category, Iterable<Income>> ret = {};
+    Iterable<Category> categories = _accountmanager
+        .getAllCategories()
+        .where((cate) => category == null ? true : cate == category);
+
+    for (var category in categories) {
+      var e = category.sollT.map((elem) => Income(elem, category));
+      if (e.isNotEmpty) {
+        ret[category] = e;
+      }
+    }
+    return ret;
+  }
+
+  List<TransactionInfo> getAllTransactionsRealtedToCategories() {
+    List<TransactionInfo> ret = [];
+    ret.addAll(_accountmanager.getAllCategories().expand((cate) => cate.habenT
+        .map((transaction) =>
+            TransactionInfo(TransactionTypes.expense, transaction, cate))));
+
+    ret.addAll(_accountmanager.getAllCategories().expand((cate) => cate.sollT
+        .map((transaction) =>
+            TransactionInfo(TransactionTypes.income, transaction, cate))));
+    return ret;
   }
 
   List<ActiveAccount> getActiveAccounts(
